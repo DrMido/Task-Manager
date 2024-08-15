0450b0751d418029b9ba748d5a818f9ea26d452d
DrMido <51366658+DrMido@users.noreply.github.com>
2024-08-15 14:58:02 +0300

    prettier kicked in

--- Controllers/mainController.js
+++ Controllers/mainController.js
@@ -1,19 +1,19 @@
 const User = require('../models/users.js');
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ msg: 'Please enter all fields' });
-    }
-    const user = await User.findOne({ username });
-    if (!user) {
-        return res.status(400).json({ msg: 'User does not exist' });
-    }
-    const isMatch = await user.password===password;
-    if (!isMatch) {
-        return res.status(400).json({ msg: 'Invalid credentials' });
-    }
-    res.json({ user});
-}
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).json({ msg: 'User does not exist' });
+  }
+  const isMatch = (await user.password) === password;
+  if (!isMatch) {
+    return res.status(400).json({ msg: 'Invalid credentials' });
+  }
+  res.json({ user });
+};
 
-module.exports = { login }
+module.exports = { login };
