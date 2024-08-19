1e3a4c910bab631e5a6dfed098a01efa258b7a99
DrMido <51366658+DrMido@users.noreply.github.com>
2024-08-19 12:11:52 +0300

    Dima's day

--- models/Task.js
+++ models/Task.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    },
-    date:{
-        type: Date,
-        default: Date.now(),
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    priority: {
-        type: String,
-        enum: ['low', 'medium', 'high'],
-        default: 'low',
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'low',
+  },
 });
 
-module.exports = mongoose.model('Task', taskSchema)
+module.exports = mongoose.model('Task', taskSchema);
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,8 +1,73 @@
 const express = require('express');
-const User = require('../models/User');
+const userModel = require('../models/User');
 const validateregister = require('../Middleware/register');
 const validateLogin = require('../Middleware/login');
 const encrypt = require('../Middleware/encryption');
 const router = express.Router();
 
-const {assignToken, verifyToken} = require('../config/token');
+const { assignToken } = require('../config/token');
+
+router.post('/register', validateregister, async (req, res) => {
+  const result = validateregister(req.body);
+  if (!result.success) {
+    return res
+      .status(200)
+      .json({ success: result.success, errors: result.errors });
+  }
+  userModel.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res
+        .status(200)
+        .json({ success: false, errors: ['Email already in use'] });
+    }
+  });
+
+  const newUser = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    password: encrypt(req.body.password),
+  });
+  let User = await newUser.save();
+  return res.status(200).json({
+    success: true,
+    Message: 'User created successfully',
+    token: assignToken(User),
+    user: {
+      username: User.username,
+      email: User.email,
+    },
+  });
+});
+
+router.post('/login', validateLogin, async (req, res) => {
+  const result = validateLogin(req.body);
+  if (!result.success) {
+    return res
+      .status(200)
+      .json({ success: result.success, errors: result.errors });
+  }
+
+  userModel.findOne({ email: req.body.email }).then((user) => {
+    if (!user) {
+      return res
+        .status(200)
+        .json({ success: false, errors: ['Email not found'] });
+    }
+    if (!user.authenticate(req.body.password)) {
+      return res.status(200).json({
+        success: false,
+        errors: ['Invalid email or password'],
+      });
+    }
+    const token = assignToken(user);
+    return res.status(200).json({
+      success: true,
+      Message: 'User logged in successfully',
+      token: token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    });
+  });
+});
